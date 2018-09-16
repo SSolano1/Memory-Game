925c2a8a39cbd18f1b0330d653741c5713a6330d
Solon Solano <ssolano1@msn.com>
2018-09-15 23:17:09 -0400

    card shuffle works

--- memorization/src/App.js
+++ memorization/src/App.js
@@ -3,7 +3,7 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
-
+import arrayShuffle from "array-shuffle";
 import "./App.css";
 
 class App extends Component {
@@ -16,47 +16,51 @@ class App extends Component {
     clicked: [],
   };
 
-  // function shuffleFriends(array) {
-  //   for (let i = array.length - 1; i > 0; i--) {
-  //     let j = Math.floor(Math.random() * (i + 1));
-  //     [array[i], array[j]] = [array[j], array[i]];
+  shuffleImg = ()=> {
+    console.log("Hello World");
+    const shuffled = arrayShuffle(friends);
+    this.setState({friends: shuffled})
+    // for (let i = array.length - 1; i > 0; i--) {
+    //   let j = Math.floor(Math.random() * (i + 1));
+    //   [array[i], array[j]] = [array[j], array[i]];
+
+    // }
+    // return array;
+  };
+
+  // handleClick = id => {
+  //   if (this.state.clicked.indexOf(id) === -1) {
+  //     this.handleIncrement();
+  //     this.setState({ clicked: this.state.clicked.concat(id) });
+  //   } else {
+  //     this.handleReset();
   //   }
-  //   return array;
   // };
 
- handleClick = id => {
-    if (this.state.clicked.indexOf(id) === -1) {
-      this.handleIncrement();
-      this.setState({ clicked: this.state.clicked.concat(id) });
-    } else {
-      this.handleReset();
-    }
-  };
+  // handleIncrement = () => {
+  //   const newScore = this.state.currentScore + 1;
+  //   this.setState({
+  //     currentScore: newScore,
+  //     rightWrong: ""
+  //   });
+  //   if (newScore >= this.state.topScore) {
+  //     this.setState({ topScore: newScore });
+  //   }
+  //   else if (newScore === 12) {
+  //     this.setState({ rightWrong: "You win!" });
+  //   }
+  //   this.handleShuffle();
+  // };
 
-  handleIncrement = () => {
-    const newScore = this.state.currentScore + 1;
-    this.setState({
-      currentScore: newScore,
-      rightWrong: ""
-    });
-    if (newScore >= this.state.topScore) {
-      this.setState({ topScore: newScore });
-    }
-    else if (newScore === 12) {
-      this.setState({ rightWrong: "You win!" });
-    }
-    this.handleShuffle();
-  };
-
-  handleReset = () => {
-    this.setState({
-      currentScore: 0,
-      topScore: this.state.topScore,
-      rightWrong: "Glaven!",
-      clicked: []
-    });
-    this.handleShuffle();
-  };
+  // handleReset = () => {
+  //   this.setState({
+  //     currentScore: 0,
+  //     topScore: this.state.topScore,
+  //     rightWrong: "Glaven!",
+  //     clicked: []
+  //   });
+  //   this.handleShuffle();
+  // };
 
   // handleShuffle = () => {
   //   let shuffledFriends = shuffleFriends(friends);
@@ -78,7 +82,8 @@ removeFriend = id => {
         <Title>Women of Marvel</Title>
         {this.state.friends.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+          imgClick = {this.shuffleImg}
+            
             id={friend.id}
             key={friend.id}
             name={friend.name}
@@ -86,9 +91,11 @@ removeFriend = id => {
 
           />
         ))}
-      </Wrapper>
+        </Wrapper>
+      
     );
   }
 }
 
 export default App;
+
--- memorization/src/components/FriendCard/FriendCard.js
+++ memorization/src/components/FriendCard/FriendCard.js
@@ -3,18 +3,17 @@ import "./FriendCard.css";
 
 
 const FriendCard = props => (
-  <div 
-  className="card" 
+  <div className= "card col-sm-3"
   value={props.id} 
-  onClick={() => props.handleClick(props.id)}
+  onClick={props.imgClick}
 >
-  <div className="card-img">
+  <div className="card-img text-center">
     <div className="img-container">
       <img alt={props.name} src={props.image} />
       
     </div>
  </div>
-  </div>
+   </div>
 );
 
 export default FriendCard;
